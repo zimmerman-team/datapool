c362a5bd3301d2fddbb4b0e4efd6f468e84fffa0
Daniel Versteegh <daniel@bitofpepper.com>
2015-07-08 13:40:21 +0200

    aded tweets per day

--- site/datapool/urls.py
+++ site/datapool/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.conf import settings
+from django.db.models import Count
 
 admin.autodiscover()
 
@@ -14,6 +15,7 @@ urlpatterns = i18n_patterns('',
         {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^select2/', include('django_select2.urls')),
     url(r'^dolly_data','dp_management.views.get_dolly_data'),
+    url(r'^tweets_per_day','dp_management.views.get_tweets_per_day'),
     url(r'^', include('cms.urls')),
 
 )
--- site/dp_management/views.py
+++ site/dp_management/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from models import DollyData,TwitterUser
+from django.db.models import Count
+import json
+
 # Create your views here.
 
 
@@ -10,5 +13,15 @@ def get_dolly_data(request):
 	start_date = request.GET['start']
 	end_date = request.GET['end']
 	search_string = request.GET['search']
-	data = DollyData.objects.filter(create_at__range=[start_date, end_date]).filter(text__search=search_string)
+	data = DollyData.objects.filter(create_at__range=[start_date, end_date])[:1000]
 	return HttpResponse(serializers.serialize("json", data))
+
+def get_tweets_per_day(request):
+	start_date = request.GET['start']
+	end_date = request.GET['end']
+	search_string = request.GET['search']
+	data = DollyData.objects.filter(create_at__range=[start_date, end_date]).extra(select={'day': 'date( create_at )'}).values('day').annotate(tweets=Count('create_at')).order_by('day')
+	return_data = []
+	for row in  data:
+		return_data.append({'tweets':row['tweets'],'date': str(row['day'])})
+	return HttpResponse(json.dumps(return_data))
