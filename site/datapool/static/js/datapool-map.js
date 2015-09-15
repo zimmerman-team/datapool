7c52918ce612680ce903137ca149b340bea36564
daanzap <daniel@zimmermanzimmerman.nl>
2015-09-15 13:37:16 +0200

    heat map almost working

--- site/datapool/static/js/datapool-map.js
+++ site/datapool/static/js/datapool-map.js
@@ -58,6 +58,7 @@ function DatapoolMap(){
             blur: 25,
             gradient: {0.5: 'rgba(204,204,0,0.1)', 0.7: 'rgba(255,102,0,0.1)', 1: 'rgba(204,0,0,0.1)'}
         }).addTo(this.map);
+        this.refresh(false,0);
     };
 
     this.resetData = function(){
@@ -79,7 +80,7 @@ function DatapoolMap(){
 
     this.createUrl = function(heatmapId){
 
-        var url = '/get_project_chart_data/'+this.pro+'/heat_map/';
+        var url = '/get_project_chart_data/'+this.project_id+'/heat_map/';
         console.log('get url = '+url);
         return url;
     }
@@ -94,20 +95,67 @@ function DatapoolMap(){
             dataType: 'json',
             data:$("#all-data").serialize(),
             success: function(data){
-                that.formatData(data);
+                data = that.formatData(data);
                 that.refresh(data, heatmapId);
             }
         });
     };
 
     this.formatData = function(data){
-        console.log(data)
+        var formattedData = {};
+        this.heat = [];
+        for(var j = 0;j < data.length;j++){
+
+            this.heat[j] = L.heatLayer([], {
+            minOpacity: .5,
+            radius: 18,
+            blur: 25,
+            gradient: {0.5: 'rgba(0,255,255,0.1)', 0.7: 'rgba(0,153,255,0.1)', 1: 'rgba(51,102,255,0.1)'}
+            }).addTo(this.map);
+
+            data_set = data[j]['data'];
+            this.search_boxes = data[j]['search_boxes'];
+            types = data[j]['property_type_set'];
+            console.log(data[j]);
+            lat_field = '';
+            long_field = '';
+            for(prop in types){
+                //console.log(types);
+                if (types[prop]['type'] == 'LAT'){
+                    lat_field = prop;
+
+                }
+                if (types[prop]['type'] == 'LONG'){
+                    long_field = prop;
+
+                }
+
+            }
+            formattedData[j] = {}
+            formattedData[j]['data_set'] = []
+            for(var i = 0; i < data_set.length;i++){
+                
+                    formattedData[j]['data_set'][i] = {}
+
+                    formattedData[j]['data_set'][i]['latitude'] = data_set[lat_field];
+                    formattedData[j]['data_set'][i]['latitude'] = data_set[long_field];
+                
+            }
+        }
+        
+        //console.log(return_arr);
+        return formattedData;
+
 
     }
     this.reDraw = function(data, heatmapId){
-
+        console.log('formattedData = ');
+        console.log(data);
         for (var i = 0;i< data.length;i++){
-            this.heat[heatmapId].addLatLng([data[i]['fields']['latitude'], data[i]['fields']['longitude']]);
+            data_set = data[i]['data_set']
+            for (var j = 0 ;j<data_set.lenght;j++){
+                this.heat[i].addLatLng([data_set[j]['latitude'], data_set[j]['longitude']]);
+            }
 
         }
 
